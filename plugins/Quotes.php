ed0c6f169e213ce87e0d8d4fd3a1b80a9e482d14
Joaquin Varela <MakotoYuki@users.noreply.github.com>
2015-11-25 14:27:40 -0300

    WAKE ME UP

--- plugins/Quotes.php
+++ plugins/Quotes.php
@@ -107,7 +107,7 @@ class Quotes extends PlugIRC_Core{
 		$quote = $this->getQuote($id, $quoter, $time);
 		return FontIRC::bold("Quote $id").': '.
 			FontIRC::terminate($quote).' :: Added by '.
-			FontIRC::italic($quoter).' on '.FontIRC::italic(date('j/n/Y H/i', $time));
+			FontIRC::italic($quoter).' on '.FontIRC::italic(date('j/n/Y H:i', $time));
 	}
 
 	public function getQuoteCount(){
